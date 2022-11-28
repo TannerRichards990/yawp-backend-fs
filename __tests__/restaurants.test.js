6eed24a0917cff014fdc4d7c6cc18348db9a8550
Tanner Richards <Tanner.richards990@gmail.com>
2022-11-28 14:31:42 -0800

    built out get all resturants in controller, model, and wrote test

---
+++ __tests__/restaurants.test.js
@@ -0,0 +1,31 @@
+const pool = require('../utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  username: 'test',
+  email: 'test@test.com',
+  password: '123456',
+};
+
+const agent = request.agent(app);
+
+describe('backend-express-template-routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('#GET /restaurants should return a list of restaurants', async () => {
+    const res = await request(app).get('/api/v1/restaurants');
+    expect(res.status).toBe(200);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+    });
+  });
+});
