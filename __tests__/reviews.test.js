357189d5947d00e0268415406535d70d6a9847c8
Tanner Richards <Tanner.richards990@gmail.com>
2022-11-30 10:56:23 -0800

    still working on getting delete test to pass

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -9,32 +9,16 @@ describe('delete tests', () => {
     return setup(pool);
   });
 
-  const mockUser = {
-    firstName: 'admin',
-    lastName: 'admin',
-    email: 'admin',
-    password: 'admin',
-  };
   
-  const registerAndLogin = async (userProps = {}) => {
-    const password = userProps.password ?? mockUser.password;
-  
-    // Create an "agent" that gives us the ability
-    // to store cookies between requests in a test
-    const agent = request.agent(app);
-  
-    // Create a user to sign in with
-    const user = await UserService.create({ ...mockUser, ...userProps });
-  
-    // ...then sign in
-    const { email } = user;
-    await agent.post('/api/v1/users/sessions').send({ email, password });
-    return [agent, user];
-  };
 
   it('admin can delete a review they posted', async () => {
-    const [agent, user] = await registerAndLogin();
-    
+    const agent = request.agent(app);
+    await UserService.create({
+      firstName: 'admin',
+      lastName: 'admin',
+      email: 'admin',
+      password: 'admin',
+    });
     await agent.post ('/api/v1/users/sessions').send({
       email: 'admin',
       password: 'admin',
